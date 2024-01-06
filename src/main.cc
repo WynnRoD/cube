ed9c67788f1fa8e6b43d57e782a021560884da33
Dickson Duah <newleaf.dd@gmail.com>
2024-01-06 15:21:34 +0000

    Increase columns

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 
+
 namespace cube {
 
 using diagonals = int const (&)[n_diagonals];
@@ -14,6 +15,17 @@ void append_attacks(bitboard, std::ostream&, diagonals);
 void append_betweens(bitboard, std::ostream&, diagonals);
 
 } // namespace cube
+
+// Number of columns to print 
+// NOTE: powers of two in the range [n_diagonals, n_total_squares]
+//
+// for i in rows
+//  for j in cols
+//    i * cols + j = i,j
+//
+// i = x + w * y <==> x = i % w, y = i / w
+
+constexpr int breakline = cube::n_total_squares >> 2;
 
 int main(int argc, char const *const argv[]) {
   static_assert(cube::board6ds_layout::rank<9>::value ==
@@ -82,7 +94,7 @@ int main(int argc, char const *const argv[]) {
 namespace cube {
 
 void append_ranks(int _MinDiag, std::ostream& _Os) {
-  _Os << "/* ranks */\n";
+  _Os << "/* Ranks */\n";
   for (auto __idx = 0; __idx < n_rows_or_cols; ++__idx)
     _Os << bitboard{rank(_MinDiag, __idx)}
         << ((__idx < n_rows_or_cols - 1) ? ",\n" : "\n");
@@ -90,7 +102,7 @@ void append_ranks(int _MinDiag, std::ostream& _Os) {
 }
 
 void append_numbers(bitboard _Dark, std::ostream& _Os) {
-  _Os << "/* standard notation */\n";
+  _Os << "/* Standard Notation */\n";
   for (auto __x = 0, num = 0; __x < n_total_squares; ++__x) {
     if (__x && __x < n_total_squares)
       _Os << ", ";
@@ -104,8 +116,10 @@ void append_numbers(bitboard _Dark, std::ostream& _Os) {
 }
 
 void append_nexts(bitboard _Dark, std::ostream& _Os, diagonals _Distances) {
+#if 0
 
-  _Os << "/* next square */\n";
+  _Os << "/* Next Square (square, diagonal) */\n";
+
   for (auto __x = 0; __x < n_total_squares; ++__x) {
     bitboard __s = 1ull << __x;
 
@@ -119,7 +133,55 @@ void append_nexts(bitboard _Dark, std::ostream& _Os, diagonals _Distances) {
 
     _Os << ((__x < n_total_squares - 1) ? ",\n" : "\n");
   }
+
+#else
+
+  int next[n_total_squares][n_diagonals];
+  mem::initialise(next, invalid, sizeof(next));
+
+  for (auto __x = 0; __x < n_total_squares; ++__x) {
+    bitboard __s = 1ull << __x;
+
+    if (_Dark.has(__s))
+      for (auto const& __dx : _Distances)
+        if (_Dark.has(__s.after(__dx)))
+          next[__x][(&__dx - _Distances)] = __x + __dx;
+  }
+
+  #if USE_DIAGS_AS_ROWS
+  constexpr int rows = n_diagonals;
+  constexpr int cols = n_total_squares;
+  #else
+  constexpr int rows = n_total_squares;
+  constexpr int cols = n_diagonals;
+  #endif
+
+  #if USE_DIAGS_AS_ROWS
+  _Os << "/* Next Square (diagonal, square) */\n";
+  #else
+  _Os << "/* Next Square (square, diagonal) */\n";
+  #endif
+
+  for (auto r = 0; r < rows; ++r) {
+    for (auto c = 0; c < cols; ++c) {
+  #if USE_DIAGS_AS_ROWS
+      int value = next[c][r];
+  #else
+      int value = next[r][c];
+  #endif
+      const int i = r * cols + c;
+      if (i) {
+        if (i < rows * cols)
+          _Os << ", ";
+        if (i % breakline == 0)
+          _Os << "\n";
+      }
+      _Os << std::setw(2) << std::setfill(' ') << value;
+    }
+  }
   _Os << "\n\n";
+
+#endif
 }
 
 void append_attacks(bitboard _Dark, std::ostream& _Os, diagonals _Distances) {
@@ -140,10 +202,10 @@ void append_attacks(bitboard _Dark, std::ostream& _Os, diagonals _Distances) {
 
   constexpr auto last = n_total_squares;
 
-  _Os << "/* attack squares */\n";
+  _Os << "/* Attack Squares (square, diagonal) */\n";
 
   for (auto __x = 0; __x < last; ++__x)
-    _Os << att[__x][0] << ", " << att[__x][1] << "," << att[__x][2] << ", "
+    _Os << att[__x][0] << ", " << att[__x][1] << ", " << att[__x][2] << ", "
         << att[__x][3] << ((__x < last - 1) ? ",\n" : "\n");
 
   _Os << "\n\n";
@@ -167,14 +229,16 @@ void append_betweens(bitboard _Dark, std::ostream& _Os, diagonals _Distances) {
         btw[__m.index()][__dst.index()] = ray(__m.index(), __dst.index(), __dx);
 
   constexpr auto last = n_total_squares;
-  constexpr auto n    = 4;
+  constexpr auto n    = 8;
 
-  _Os << "/* squares between */\n";
+  _Os << "/* Between Squares (s0, s1) */\n";
 
   for (auto __x = 0; __x < last; ++__x)
     for (auto __y = 0; __y < last; __y += n)
       _Os << btw[__x][__y + 0] << ", " << btw[__x][__y + 1] << ", "
-          << btw[__x][__y + 2] << ", " << btw[__x][__y + 3]
+          << btw[__x][__y + 2] << ", " << btw[__x][__y + 3] << ", "
+          << btw[__x][__y + 4] << ", " << btw[__x][__y + 5] << ", "
+          << btw[__x][__y + 6] << ", " << btw[__x][__y + 7]
           << ((__x < last - 1 || last - n > __y) ? ",\n" : "\n");
 
   _Os << "\n\n";
